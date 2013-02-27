27ee0ee5dd5052d805dc824704660f3ef5bc53b4
Monnoroch <monnoroch@gmail.com>
2013-02-27 12:36:37 +0400

    (new) new version of a plugin

--- ColorHighlighter.py
+++ ColorHighlighter.py
@@ -1,18 +1,18 @@
 import sublime, sublime_plugin
 import os
+import re
+import string
 
 import colors
 
-version = "2.1.0"
+version = "3.0"
 
 # Constants
 PACKAGES_PATH = sublime.packages_path()
 
-dec_digits = "0123456789" #['0','1','2','3','4','5','6','7','8','9']
+hex_digits = string.digits + "ABCDEF"
+letters = string.digits + string.letters
 
-hex_digits = dec_digits + "ABCDEFabcdef" #[ 'A','B','C','D','E','F','a','b','c','d','e','f']
-
-MAX_COL_LEN = 16
 loglist = ["Version: " + version]
 PREFIX = "mcol_"
 sets_name = "ColorHighlighter.sublime-settings"
@@ -34,6 +34,204 @@ def read_file(fl):
 	res = f.read()
 	f.close()
 	return res
+
+
+# Color formats:
+# #FFFFFFFF
+# #FFFFFF
+# #FFF
+# rgb(255,255,255)
+# words
+# black
+
+max_len = max(len("#FFB"), len("#FFFFFF"), len("#FFFFFFAA"), len("rgb(199,255,255)"))
+
+regex1 = re.compile("[r][g][b][(]\d{1,3}[,]\d{1,3}[,]\d{1,3}[)]")
+regex2 = re.compile("[#][\dA-F]{8}")
+regex3 = re.compile("[#][\dA-F]{6}")
+regex4 = re.compile("[#][\dA-F]{3}")
+
+
+def tohex(r,g,b):
+	sr = "%X" % r
+	if len(sr) == 1:
+		sr = '0' + sr
+	sg = "%X" % g
+	if len(sg) == 1:
+		sg = '0' + sg
+	sb = "%X" % b
+	if len(sb) == 1:
+		sb = '0' + sb
+	return "#%s%s%sFF" % (sr,sg,sb)
+
+def isInColorS(s, pos):
+	m = regex1.search(s)
+	if m != None and m.group(0) != None and m.start(0) <= pos and m.end(0) >= pos:
+		s = m.group(0)
+		s = s[3+1:-1]
+		n = s.find(",")
+		r = s[0:n]
+		s = s[n+1:]
+		n = s.find(",")
+		g = s[0:n]
+		s = s[n+1:]
+		b = s
+		return sublime.Region(m.start(0), m.end(0)), tohex(int(r), int(g), int(b))
+
+	s = s.upper()
+
+	# check #FFFFFFFF
+	m = regex2.search(s)
+	if m != None and m.group(0) != None and m.start(0) <= pos and m.end(0) >= pos:
+		return sublime.Region(m.start(0), m.end(0)), m.group(0)
+
+	# check #FFFFFF
+	m = regex3.search(s)
+	if m != None and m.group(0) != None and m.start(0) <= pos and m.end(0) >= pos:
+		return sublime.Region(m.start(0), m.end(0)), m.group(0) + "FF"
+
+	# check #FFF
+	m = regex4.search(s)
+	if m != None and m.group(0) != None and m.start(0) <= pos and m.end(0) >= pos:
+		s = m.group(0)
+		return sublime.Region(m.start(0), m.end(0)), s[0] + s[1]*2 + s[2]*2 + s[3]*2 + "FF"
+
+	return None, None
+
+def get_current_word(view, sel):
+	n = sel.begin() - 1
+	k = sel.end()
+	while k - n <= max_len and view.substr(n) in letters:
+		n -= 1
+	while k - n <= max_len and view.substr(k) in letters:
+		k += 1
+	return sublime.Region(n + 1, k)
+
+def isInColor(view, sel):
+	b = sel.begin()
+	if b != sel.end():
+		return None, None
+
+	wd = get_current_word(view, sel)
+	res = colors.names_to_hex.get(view.substr(wd))
+	if res != None:
+		return wd, res
+
+	for i in range(1, max_len):
+		s = view.substr(sublime.Region(b - i, b + i))
+		wd, res = isInColorS(s, i)
+		if res != None:
+			return sublime.Region(wd.begin() + (b - i), wd.end() + (b - i)), res
+
+	return None, None
+
+class HtmlGen:
+	colors = []
+	color_scheme = None
+	need_upd = False
+	need_restore = False
+	need_backup = False
+	string = ""
+	gen_string = """
+<dict>
+<key>name</key>
+<string>mon_color</string>
+<key>scope</key>
+<string>%s</string>
+<key>settings</key>
+<dict>
+<key>background</key>
+<string>%s</string>
+<key>caret</key>
+<string>%s</string>
+<key>foreground</key>
+<string>%s</string>
+</dict>
+</dict>\n
+"""
+
+	def get_y(self, col):
+		return (0.3 * int(col[1:3],16) + 0.59 * int(col[3:5],16) + 0.11 * int(col[5:7],16)) * (int(col[7:9],16) / 255.0)
+
+	def get_cont_col(self, col):
+		if self.get_y(col) > 255.0/2:
+			return "#000000FF"
+		return "#FFFFFFFF"
+
+	def region_name(self, s):
+		return PREFIX + s[1:]
+
+	def add_color(self, col):
+		if col in self.colors: return
+		self.colors.append(col)
+		cont = self.get_cont_col(col)
+		self.string += self.gen_string % (self.region_name(col), col, cont, cont)
+		self.need_upd = True
+
+	def need_update(self):
+		return self.need_upd
+
+	def update(self, view):
+		if not self.need_upd: return
+		self.need_upd = False
+
+		cs = self.color_scheme
+		# do not support empty color scheme
+		if cs == "":
+			log("Empty scheme, can't backup")
+			return
+		# extract name
+		cs = cs[cs.find('/'):]
+		cont = None
+		if os.path.exists(PACKAGES_PATH + cs + ".chback"):
+			cont = read_file(PACKAGES_PATH + cs + ".chback")
+			log("Already backuped")
+		else:
+			cont = read_file(PACKAGES_PATH + cs)
+			write_file(PACKAGES_PATH + cs + ".chback", cont) # backup
+			log("Backup done")
+
+		self.string = self.string.decode("utf-8").encode("utf-8")
+
+		# edit cont
+		n = cont.find("<array>") + len("<array>")
+		cont = cont[:n] + self.string + cont[n:]
+
+		write_file(PACKAGES_PATH + cs, cont)
+		self.need_restore = True
+
+	def restore_color_scheme(self):
+		if not self.need_restore: return
+		self.need_restore = False
+
+		cs = self.color_scheme
+		# do not support empty color scheme
+		if cs == "":
+			log("Empty scheme, can't restore")
+			return
+		# extract name
+		cs = cs[cs.find('/'):]
+		if os.path.exists(PACKAGES_PATH + cs + ".chback"):
+			log("Starting restore scheme: " + cs)
+			# TODO: move to other thread
+			write_file(PACKAGES_PATH + cs, read_file(PACKAGES_PATH + cs + ".chback"))
+			log("Restore done.")
+		else:
+			log("No backup :(")
+
+	def set_color_scheme(self, view):
+		self.color_scheme = view.settings().get('color_scheme')
+		self.need_backup = True
+
+
+	def change_color_scheme(self, view):
+		cs = view.settings().get('color_scheme')
+		if cs != self.color_scheme:
+			self.restore_color_scheme()
+			self.set_color_scheme(view)
+			self.update(view)
+
+htmlGen = HtmlGen()
 
 # Commands
 
@@ -69,266 +267,36 @@ class chlogCommand(sublime_plugin.TextCommand):
 # command to restore color scheme
 class RestoreColorSchemeCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		cs = self.view.settings().get('color_scheme')
-		# do not support empty color scheme
-		if cs == "":
-			log("Empty scheme, can't restore")
-			return
-		# extract name
-		cs = cs[cs.find('/'):]
-		if os.path.exists(PACKAGES_PATH + cs + ".chback"):
-			log("Starting restore scheme: " + cs)
-			write_file(PACKAGES_PATH + cs, read_file(PACKAGES_PATH + cs + ".chback"))
-			log("Restore done.")
-			all_colors.deinit()
-		else:
-			log("No backup :(")
+		htmlGen.restore_color_scheme(self.view)
 
-class ColorContainer:
-
-	colors = []
-	newcolors = []
-	string = ""
-	gen_string = "<dict><key>name</key><string>mon_color</string><key>scope</key><string>%s\
-</string><key>settings</key><dict><key>background</key><string>%s\
-</string><key>caret</key><string>%s\
-</string><key>foreground</key><string>%s\
-</string></dict></dict>\n"
-
-# rgb(255,1,10)
-
-	def __init__(self):
-		pass
-
-	def add(self,col):
-		self.newcolors.append(col)
-
-	def generate_string(self, col):
-		cont = get_cont_col(col)
-		self.string += self.gen_string % (region_name(col), col, cont, cont)
-
-	def update(self):
-		res = False
-		for c in self.newcolors:
-			if c not in self.colors:
-				self.colors.append(c)
-				self.generate_string(c)
-				res = True
-		self.newcolors = []
-		self.string = self.string.decode("utf-8").encode("utf-8")
-		return res
-
-	def need_update(self):
-		return self.newcolors != []
-
-	def deinit(self):
-		self.colors = []
-		self.newcolors = []
-		self.string = "".decode("utf-8").encode("utf-8")
-
-all_colors = ColorContainer()
-
-def region_name(s):
-	return PREFIX + s[1:]
-
-def tohex(r,g,b):
-	sr = "%X" % r
-	if len(sr) == 1:
-		sr = '0' + sr
-	sg = "%X" % g
-	if len(sg) == 1:
-		sg = '0' + sg
-	sb = "%X" % b
-	if len(sb) == 1:
-		sb = '0' + sb
-	return "#%s%s%sFF" % (sr,sg,sb)
-
-def tolong(col):
-	ln = len(col)
-	if ln == 9:
-		return col.upper()
-	if ln == 7:
-		return col.upper() + "FF"
-	return ("#%s%s%sFF" % (col[1]*2, col[2]*2, col[3]*2)).upper()
-
-
-def isColor(col):
-	ln = len(col)
-	if ln < 3 or ln > MAX_COL_LEN:
-		return False
-	if col[0] == '#':
-		if ln not in [4,7,9]:
-			return False
-		for l in col[1:]:
-			if l not in hex_digits:
-				return False
-		return tolong(col)
-	if ch_settings.get("0x_hex_values") and col[:2] == "0x":
-		if ln in [5,8,10]:
-			for l in col[2:]:
-				if l not in hex_digits:
-					return False
-			return tolong('#' + col[2:])
-		return False
-	if ch_settings.get("hex_values") and ln in [3,6,8]:
-		for l in col:
-			if l not in hex_digits:
-				return False
-		return tolong('#' + col)
-	if col[-1] != ')' or col[0:4] != "rgb(":
-		return False
-	cols = col[4:-1].split(',')
-	if len(cols) != 3:
-		return False
-	for n in cols:
-		ll = len(n)
-		if ll > 3 or ll == 0:
-			return False
-		for c in n:
-			if c not in dec_digits:
-				return False
-	return tohex(int(cols[0]),int(cols[1]),int(cols[2]))
-
-def get_y(col):
-	return (0.3 * int(col[1:3],16) + 0.59 * int(col[3:5],16) + 0.11 * int(col[5:7],16)) * (int(col[7:9],16) / 255.0)
-
-def get_cont_col(col):
-	if get_y(col) > 255.0/2:
-		return "#000000FF"
-	return "#FFFFFFFF"
 
 class ColorSelection(sublime_plugin.EventListener):
-    #000000
-    #FFFFFFFF
-    # rgb(255,255,255)
-
-	colors = all_colors
-	color_scheme = None
-	color_scheme_cont = None
-	process = False
-
-	letters = hex_digits + "#(),rgbx" # ['#','(', ')',',','r','g','b']
-
-
-	def get_current_word(self, view, sel):
-		n = sel.begin() - 1
-		k = sel.end()
-		while k - n <= MAX_COL_LEN and view.substr(n) in self.letters:
-			n -= 1
-		while k - n <= MAX_COL_LEN and view.substr(k) in self.letters:
-			k += 1
-		return sublime.Region(n + 1,k)
-
-	def _stop_process(self):
-		log("Stopped!")
-		self.process = False
-
-	def stop_process(self):
-		self._stop_process()
-		#sublime.set_timeout(lambda self = self : self._stop_process(), 200)
-
-	def start_process(self):
-		log("Started!")
-		self.process = True
-
-	def modify_color_scheme(self, view):
-		if self.color_scheme == None:
-			self.color_scheme_change(view)
-			log("color_scheme is none")
-			return False
-		if self.process:
-			# try later
-			log("Alreaty doing something")
-			# TODO: decide if needed
-			sublime.set_timeout(lambda self = self, view = view : self.modify_color_scheme(view), 100)
-			return False
-		log("Modifying scheme: " + self.color_scheme)
-		self.start_process()
-		ss = self.color_scheme_cont[0] + self.colors.string + self.color_scheme_cont[1]
-		write_file(PACKAGES_PATH + self.color_scheme, ss)
-		self.stop_process()
-		log("Modifying done.")
-		
-	def read_colors(self, s):
-		self.colors.deinit()
-		n = s.find(PREFIX)
-		while n != -1:
-			s = s[n+5:]
-			self.colors.add('#' + s[:8])
-			n = s.find(PREFIX)
-		log("Colors loaded: " + str(self.colors.newcolors))
-		self.colors.update()
-
-	def _color_scheme_change(self, view, cs):
-		log("Changing to scheme: " + cs)
-		self.color_scheme = cs
-		self.start_process()
-		cont = read_file(PACKAGES_PATH + cs)
-
-		# backup the theme
-		if not os.path.exists(PACKAGES_PATH + cs + ".chback"):
-			log("Backup scheme: " + cs)
-			write_file(PACKAGES_PATH + cs + ".chback", cont)
-			log("Backup done!")
-		
-		n = cont.find("<array>") + 7
-		self.color_scheme_cont = [cont[:n], cont[n:]]
-		self.read_colors(cont[n:cont.rfind(PREFIX)+8+5])
-		self.stop_process()
-		log("Changing done.")
-		self.modify_color_scheme(view)
-
-	def color_scheme_change(self, view):
-		# getting the color file path (cs)
-		cs = view.settings().get('color_scheme')
-		# do not support empty color scheme
-		if cs == "":
-			log("Empty scheme")
-			return
-		# extract name
-		cs = cs[cs.find('/'):]
-		# nothing's changed
-		if self.color_scheme == cs:
-			return
-		log("Loaded scheme: " + cs)
-		if self.process:
-			log("Something's really wrong!")
-			return
-		self._color_scheme_change(view, cs)
-
 	def on_new(self, view):
 		sets = view.settings()
-		sets.add_on_change("color_scheme", lambda self = self, view = view : self.color_scheme_change(view))
-		self.color_scheme_change(view)
+		htmlGen.set_color_scheme(view)
+		sets.add_on_change("color_scheme", lambda self = self, view = view : htmlGen.change_color_scheme(view))
+		# htmlGen.change_color_scheme(view)
 
 	def on_clone(self, view):
 		self.on_new(view)
 
-		black
+	def on_close(self, view):
+		htmlGen.restore_color_scheme(view)
 
 	def on_selection_modified(self, view):
 		selection = view.sel()
 		words = []
 		for s in selection:
-			wd = self.get_current_word(view,s)
-			col = isColor(view.substr(wd))
-			if not col:
-				wd = view.word(s)
-				try:
-					col = colors.names_to_hex[view.substr(wd)]
-				except KeyError:
-					pass
-			if col:
-				self.colors.add(col)
-				words.append((wd,col))
-		if self.colors.update():
-			#sublime.set_timeout(lambda self = self, view = view : self.modify_color_scheme(view), 0)
-			self.modify_color_scheme(view)
-		if words == []:
-			view.erase_regions("mon_CH")
-			return
+			wd, col = isInColor(view, s)
+			if col != None:
+				htmlGen.add_color(col)
+				words.append((wd, col))
+
+		if htmlGen.need_update():
+			htmlGen.update(view)
+
+		view.erase_regions("mon_CH")
+
 		for wd in words:
-			w,c = wd
-			#log(region_name(c))
-			view.add_regions("mon_CH",[w], region_name(c))
-			
+			w, c = wd
+			view.add_regions("mon_CH",[w], htmlGen.region_name(c))
